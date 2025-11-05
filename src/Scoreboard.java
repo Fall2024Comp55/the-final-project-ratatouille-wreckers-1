2eaf39106967d61b545d18394c099b4f5374cd35
royjj <royjj@Jimenez>
2025-11-05 15:44:03 -0800

    scoreboard

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,4 +1,46 @@
+import acm.graphics.*;
+import java.awt.Font;
 
 public class Scoreboard {
+    // --- Fields ---
+    private MainApplication mainScreen;
+    private int displayedScore;
+    private GLabel scoreLabel;
+    private boolean visible = false;
 
+    // --- Constructor ---
+    public Scoreboard(MainApplication mainScreen) {
+        this.mainScreen = mainScreen;
+        this.displayedScore = 0;
+
+        // Create a label to show the score
+        scoreLabel = new GLabel("Score: " + displayedScore);
+        scoreLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+        scoreLabel.setColor(java.awt.Color.WHITE);
+        scoreLabel.setLocation(10, 25); // top-left corner
+    }
+
+    // --- Methods ---
+    public void update(int score) {
+        displayedScore = score;
+        scoreLabel.setLabel("Score: " + displayedScore);
+    }
+
+    public void show() {
+        if (!visible) {
+            mainScreen.add(scoreLabel);
+            visible = true;
+        }
+    }
+
+    public void hide() {
+        if (visible) {
+            mainScreen.remove(scoreLabel);
+            visible = false;
+        }
+    }
+
+    public int getDisplayedScore() {
+        return displayedScore;
+    }
 }
