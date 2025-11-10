d76fd18f966d461b31d138a99e3b59ded69f33c1
elemoay <elemoay@dhcp-10-15-185-250.wireless.pacific.edu>
2025-11-10 14:21:14 -0800

    updated SettingsPane

--- src/SettingsPane.java
+++ src/SettingsPane.java
@@ -1,5 +1,8 @@
 
 public class SettingsPane extends GraphicsPane {
+	public SettingsPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
